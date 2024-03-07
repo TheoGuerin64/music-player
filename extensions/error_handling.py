34d007f9f1498d2e2a79662d881e28a44adbc2b4
TheoGuerin64 <57496441+TheoGuerin64@users.noreply.github.com>
2024-03-07 01:47:44 +0100

    Add error handling extension

---
+++ extensions/error_handling.py
@@ -0,0 +1,25 @@
+import logging
+
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+logger = logging.getLogger(__name__)
+
+
+class error_handler(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        super().__init__()
+        bot.tree.on_error = self.on_error
+
+    async def on_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError) -> None:
+        if interaction.response.is_done():
+            send = interaction.followup.send
+        else:
+            send = interaction.response.send_message
+        await send("An error occurred.", ephemeral=True)
+        logger.error(error, exc_info=True)
+
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(error_handler(bot))
