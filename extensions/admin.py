5f976d2a6b4016f536e17100eb78d6055ed15071
TheoGuerin64 <57496441+TheoGuerin64@users.noreply.github.com>
2024-03-25 11:30:25 +0100

    Add message write and reaction add commands

--- extensions/admin.py
+++ extensions/admin.py
@@ -79,6 +79,63 @@ class Admin(commands.Cog):
             db.set_one_piece_channel_id(interaction.guild.id, channel.id)
             await interaction.response.send_message(f"One Piece channel set to {channel.mention}.", ephemeral=True)
 
+    @app_commands.command()
+    @app_commands.describe()
+    @app_commands.default_permissions(administrator=True)
+    @app_commands.guild_only()
+    async def write(self, interaction: Interaction, message: str, channel: Optional[discord.TextChannel]) -> None:
+        """Write a message in a channel.
+
+        Args:
+            Message: The message to write.
+            channel: The channel.
+        """
+        if interaction.guild is None:
+            raise CommandError("This command can not be used here.", True)
+        if not isinstance(interaction.channel, discord.TextChannel):
+            raise CommandError("This command can not be used here.", True)
+
+        if not channel:
+            channel = interaction.channel
+
+        await interaction.response.defer(thinking=True, ephemeral=True)
+        await channel.send(message)
+        await interaction.followup.send("Message sent.")
+
+    @app_commands.command()
+    @app_commands.describe()
+    @app_commands.default_permissions(administrator=True)
+    @app_commands.guild_only()
+    async def add_reaction(self, interaction: Interaction, message_id: str, emoji: str) -> None:
+        """Add a reaction to a message.
+
+        Args:
+            message_id: The message id.
+            emoji: The emoji.
+        """
+        if interaction.guild is None:
+            raise CommandError("This command can not be used here.", True)
+        if not isinstance(interaction.channel, discord.TextChannel):
+            raise CommandError("This command can not be used here.", True)
+
+        if len(message_id) > 20:
+            raise CommandError("Invalid message id.", True)
+        try:
+            message_id_int = int(message_id)
+        except ValueError:
+            raise CommandError("Invalid message id.", True)
+
+        message = await interaction.channel.fetch_message(message_id_int)
+        if not message:
+            raise CommandError("Message not found.", True)
+
+        await interaction.response.defer(thinking=True, ephemeral=True)
+        try:
+            await message.add_reaction(emoji)
+        except (discord.HTTPException, discord.NotFound, TypeError):
+            raise CommandError("Invalid emoji.", True)
+        await interaction.followup.send("Reaction added.", ephemeral=True)
+
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(Admin(bot))
